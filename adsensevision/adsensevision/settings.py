0be50c28556ff00861abaffcbbde4ad0509b2667
dressgecko <dressgecko@gmail.com>
2024-04-07 06:12:27 +0500

    added modelviewsets and media settings

--- adsensevision/adsensevision/settings.py
+++ adsensevision/adsensevision/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-(moh3zg%w@!p8z!gbqo-rj+*-jrz!0ez!+5!t0uu9i!tk-d+pu
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -77,7 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'adsensevision.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -91,7 +88,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -111,7 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -123,6 +118,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Media files can be defined as those files which are uploaded by the users
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -133,3 +131,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+}
--- adsensevision/adsensevision_management_api/views.py
+++ adsensevision/adsensevision_management_api/views.py
@@ -5,78 +5,27 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Camera, CameraScreen, MediaContent, Schedule, Screen, Statistics
 from .serializers import MediaContentSerializer, CameraSerializer, ScreenSerializer
-from rest_framework.viewsets import ViewSet
+from rest_framework.viewsets import ModelViewSet
+from django.core.files.storage import default_storage
+
+
 # Create your views here.
 
 
-# Базовый класс для ViewSet для определения поведния
-class BaseViewSet(ViewSet):
-    @classmethod
-    def get_object(cls, pk):
-        try:
-            return cls.model.objects.get(pk=pk)
-        except cls.model.DoesNotExist:
-            raise status.HTTP_404_NOT_FOUND
-
-
-class MediaContentViewSet(BaseViewSet):
-    model = MediaContent
-    queryset = model.objects.all()
-    serializer_class = MediaContentSerializer
-
-    def list(self, request):
-        queryset = self.queryset
-        serializer = self.serializer_class(queryset, many=True)
-        return Response(serializer.data)
-
-    def retrieve(self, request, pk=None):
-        instance = self.get_object(pk)
-        serializer = self.serializer_class(instance)
-        return Response(serializer.data)
-
-    def destroy(self, request, pk=None):
-        instance = self.get_object(pk)
-        instance.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-class CameraViewSet(BaseViewSet):
-    model = Camera
-    queryset = model.objects.all()
+class CameraViewSet(ModelViewSet):
+    queryset = Camera.objects.all()
     serializer_class = CameraSerializer
 
-    def list(self, request):
-        queryset = self.queryset
-        serializer = self.serializer_class(queryset, many=True)
-        return Response(serializer.data)
 
-    def retrieve(self, request, pk=None):
-        instance = self.get_object(pk)
-        serializer = self.serializer_class(instance)
-        return Response(serializer.data)
-
-    def destroy(self, request, pk=None):
-        instance = self.get_object(pk)
-        instance.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-class ScreenViewSet(BaseViewSet):
-    model = Screen
-    queryset = model.objects.all()
+class ScreenViewSet(ModelViewSet):
+    queryset = Screen.objects.all()
     serializer_class = ScreenSerializer
 
-    def list(self, request):
-        queryset = self.queryset
-        serializer = self.serializer_class(queryset, many=True)
-        return Response(serializer.data)
 
-    def retrieve(self, request, pk=None):
-        instance = self.get_object(pk)
-        serializer = self.serializer_class(instance)
-        return Response(serializer.data)
+class MediaContentViewSet(ModelViewSet):
+    queryset = MediaContent.objects.all()
+    serializer_class = MediaContentSerializer
 
-    def destroy(self, request, pk=None):
-        instance = self.get_object(pk)
-        instance.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
